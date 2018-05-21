a4bda58ff458365eca0faa6a0d0e3cac6ebeb4d2
fengchao192 <fengchao192@126.com>
2018-05-21 17:04:18 +0800

    session的添加！

--- src/main/java/com/lgy/interceptor/WebSocketHandshakeInterceptor.java
+++ src/main/java/com/lgy/interceptor/WebSocketHandshakeInterceptor.java
@@ -4,16 +4,40 @@ import java.util.Map;
 
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.server.HandshakeInterceptor;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Repository
 public class WebSocketHandshakeInterceptor extends HttpSessionHandshakeInterceptor {
 
 	public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
 			Map<String, Object> attributes) throws Exception {
 		System.out.println("握手之前····");
+		if (request instanceof ServletServerHttpRequest) {
+			ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) request;
+			HttpSession session = servletRequest.getServletRequest().getSession(false);
+
+			return false;
+			//HttpSession session = servletRequest.getServletRequest().getSession(false);
+			/*//如果用户已经登录，允许聊天
+			if(session.getAttribute("loginUser")!=null){
+				//获取登录的用户
+				User loginUser=(User)session.getAttribute("loginUser") ;
+				//将用户放入socket处理器的会话(WebSocketSession)中
+				attributes.put("loginUser", loginUser);
+				System.out.println("Websocket:用户[ID:" + (loginUser.getId() + ",Name:"+loginUser.getNickname()+"]要建立连接"));
+			}else{
+				//用户没有登录，拒绝聊天
+				//握手失败！
+				System.out.println("--------------握手已失败...");
+				return false;
+			}*/
+		}
 		return super.beforeHandshake(request, response, wsHandler, attributes);
 	}
 
---
+++ src/main/webapp/main.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8" isELIgnored="false" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <a href="#">点击跳入到主界面</a>
+</body>
+</html>
